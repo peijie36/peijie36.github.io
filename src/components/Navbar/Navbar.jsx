d281636868567d6aed87f57845dbc02aea9ea3b6
peijie36 <peijiezheng2001@gmail.com>
2023-08-19 21:03:46 -0700

    Fix navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,22 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
-        <div className="flex flex-col bg-white">
-            <nav className="flex justify-around py-3 bg-white/80 backdrop-blur-md shadow-md w-full fixed top-0 left-0 right-0 z-10">
-                <div className="flex items-center">
-                    <h3 className="text-2xl font-medium text-blue-500">Logo</h3>
+        <>
+            <nav className="flex justify-between m-auto py-3 bg-white/80 backdrop-blur-md shadow-md fixed w-full top-0 left-0 right-0 z-10">
+                <div className="items-center">
+                    <h3 className="text-3xl font-medium text-blue-500 px-7">
+                        PJ
+                    </h3>
                 </div>
-                <div className="items-center hidden space-x-8 lg:flex">
+                <div className="items-center hidden space-x-10 lg:flex px-7">
                     <a className="nav-item">Home</a>
                     <a className="nav-item">Projects</a>
                     <a className="nav-item">Skills</a>
                     <a className="nav-item">Contact</a>
                 </div>
             </nav>
-        </div>
+        </>
     );
 };
 
