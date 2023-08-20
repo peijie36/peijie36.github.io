4ff8bcfdeb676c546459b10a43bbd5a32c220c2b
peijie36 <peijiezheng2001@gmail.com>
2023-08-19 21:14:01 -0700

    Add home intro page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+    return (
+        <div name="home" className="w-full h-screen bg-white">
+            {/* Container */}
+            <div className="max-w-[1000px] m-auto px-12 flex flex-col justify-center h-5/6">
+                <p className="text-black-600">Hi, my name is</p>
+                <h1 className="text-4xl sm:text-7xl font-bold text-black">
+                    Peijie Zheng
+                </h1>
+                <h2 className="text-3xl sm:text-7xl font-bold text-[#8892b0]">
+                    Full Stack Developer
+                </h2>
+            </div>
+        </div>
+    );
+};
 
-export default Home
+export default Home;
