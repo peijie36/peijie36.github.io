06bad3d015cd647415f181142d0d4cc365fa2d28
peijie36 <peijiezheng2001@gmail.com>
2023-10-09 00:20:01 -0700

    Add skills icons

--- src/Info.jsx
+++ src/Info.jsx
@@ -1,17 +1,31 @@
 import placeholder from "src/assets/placeholder.jpg";
 
-const languages = ["Java", "JavaScript", "Python", "HTML", "CSS", "Bash"];
-
-const webDev = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "ExpressJS",
-    "Tailwind CSS",
+const languages = [
+    { name: "Java", icon: <i className="devicon-java-plain colored"></i> },
+    { name: "JavaScript", icon: <i className="devicon-javascript-plain colored"></i> },
+    { name: "Python", icon: <i className="devicon-python-plain colored"></i> },
+    { name: "HTML", icon: <i className="devicon-html5-plain colored"></i> },
+    { name: "CSS", icon: <i className="devicon-css3-plain colored"></i> },
+    { name: "Bash", icon: <i className="devicon-bash-plain colored"></i> },
 ];
 
-const techTools = ["Git", "MongoDB", "Firebase", "Azure", "Postman", "Jest"];
+const webDev = [
+    { name: "JavaScript", icon: <i className="devicon-javascript-plain colored"></i> },
+    { name: "React", icon: <i className="devicon-react-original colored"></i> },
+    { name: "NodeJS", icon: <i className="devicon-nodejs-plain colored"></i> },
+    { name: "ExpressJS", icon: <i className="devicon-express-original colored"></i> },
+    { name: "Django", icon: <i className="devicon-django-plain colored"></i> },
+    { name: "Tailwind CSS", icon: <i className="devicon-tailwindcss-plain colored"></i> },
+];
+
+const techTools = [
+    { name: "Git", icon: <i className="devicon-git-plain colored"></i> },
+    { name: "MongoDB", icon: <i className="devicon-mongodb-plain colored"></i> },
+    { name: "Firebase", icon: <i className="devicon-firebase-plain colored"></i> },
+    { name: "Azure", icon: <i className="devicon-azure-plain colored"></i> },
+    { name: "SQLite", icon: <i className="devicon-sqlite-plain colored"></i> },
+    { name: "Jest", icon: <i className="devicon-jest-plain colored"></i> },
+];
 
 const projects = [
     {
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,46 +1,62 @@
 import { languages, webDev, techTools } from "src/Info";
-import "./Skills.css"
+import uniqid from "uniqid";
+import "./Skills.css";
 
 const Skills = () => {
-  return (
-      <div name="skills" className="flex flex-col m-auto pt-24 justify-normal w-full">
-          <h2 className="text-3xl text-left font-semibold text-[#646cff] mb-7 mr-32 underline underline-offset-8">
-              Skills
-          </h2>
-          <div className="flex flex-col max-sm:items-center sm:flex-row min-h-full mb-36 sm:mb-48">
-              <div className="column-container">
-                  <h3 className="container-header">Languages</h3>
-                  <ul className="mb-7">
-                      {languages.map((language) => (
-                          <li key={language} className="list-item">
-                              {language}
-                          </li>
-                      ))}
-                  </ul>
-              </div>
-              <div className="column-container mx-16">
-                  <h3 className="container-header">Web Dev</h3>
-                  <ul className="mb-7">
-                      {webDev.map((tool) => (
-                          <li key={tool} className="list-item">
-                              {tool}
-                          </li>
-                      ))}
-                  </ul>
-              </div>
-              <div className="column-container">
-                  <h3 className="container-header">Technologies</h3>
-                  <ul className="mb-7">
-                      {techTools.map((tool) => (
-                          <li key={tool} className="list-item">
-                              {tool}
-                          </li>
-                      ))}
-                  </ul>
-              </div>
-          </div>
-      </div>
-  );
-}
+    return (
+        <div name="skills" className="flex flex-col m-auto pt-24 justify-normal w-full">
+            <h2 className="text-3xl text-left font-semibold text-[#646cff] mb-7 mr-32 underline underline-offset-8">
+                Skills
+            </h2>
+            <div className="flex flex-col max-sm:items-center sm:flex-row min-h-full mb-36 sm:mb-48">
+                <div className="column-container">
+                    <h3 className="container-header">Languages</h3>
+                    <ul className="flex flex-col mb-7 list-outside">
+                        {languages.map((language) => (
+                            <li key={uniqid()} className="list-item">
+                                <div className="flex justify-center items-center">
+                                    {language.icon}
+                                    <span className="text-lg px-3">
+                                        {language.name}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="column-container mx-16">
+                    <h3 className="container-header">Web Dev</h3>
+                    <ul className="mb-7">
+                        {webDev.map((tool) => (
+                            <li key={uniqid()} className="list-item">
+                                <div className="flex justify-center items-center">
+                                    {tool.icon}
+                                    <span className="text-lg px-3">
+                                        {tool.name}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="column-container">
+                    <h3 className="container-header">Technologies</h3>
+                    <ul className="mb-7">
+                        {techTools.map((tool) => (
+                            <li key={uniqid()} className="list-item">
+                                <div className="flex justify-center items-center">
+                                    {tool.icon}
+                                    <span className="text-lg px-3">
+                                        {tool.name}
+                                    </span>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Skills
+export default Skills;
