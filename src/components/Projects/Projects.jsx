521ef187e691f1c283605572e9b4480bcdc40b5e
peijie36 <peijiezheng2001@gmail.com>
2023-10-17 22:47:48 -0700

    Fix project style

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import ProjectCard from "src/components/ProjectCard/ProjectCard";
 
 const Projects = () => {
     return (
-        <div name="projects" className="flex flex-col m-auto sm:pt-20 justify-normal w-12/13">
+        <div name="projects" className="flex flex-col m-auto pt-24 sm:pt-20 justify-normal w-12/13">
             <h2 className="text-3xl text-left font-semibold text-[#646cff] mb-7 mr-32 underline underline-offset-8">
                 PROJECTS
             </h2>
