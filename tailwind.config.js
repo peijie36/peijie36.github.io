b975acb6d33fd1075551ac5d898c1340864d7f88
peijie36 <peijiezheng2001@gmail.com>
2023-10-17 21:08:40 -0700

    Add purge files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     mode: "jit",
+    purge: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx}"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
     },
     plugins: [],
 };
-
